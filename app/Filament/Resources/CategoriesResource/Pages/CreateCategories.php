4aac743e73aec72d355e6dd4b1bb3ce412b124a4
si-aji <contact@siaji.com>
2025-04-10 09:34:09 +0700

    feat: Categories

---
+++ app/Filament/Resources/CategoriesResource/Pages/CreateCategories.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources\CategoriesResource\Pages;
+
+use App\Filament\Resources\CategoriesResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Request;
+use Filament\Support\Exceptions\Halt;
+use Throwable;
+
+class CreateCategories extends CreateRecord
+{
+    protected static string $resource = CategoriesResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $request_id = Request::header('request-id', null);
+
+        $data['request_id'] = $request_id;
+        $data['user_id'] = \Illuminate\Support\Facades\Auth::user()->id;
+
+        return $data;
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        // Check for request_id columns
+        $exists = !empty($data['request_id']) ? \App\Models\Category::where(DB::raw('BINARY `request_id`'), $data['request_id'])->first() : null;
+        $model = $exists;
+        if(empty($model)){
+            $model = static::getModel()::create($data);
+        }
+
+        return $model;
+    }
+
+    public function create(bool $another = false): void
+    {
+        $this->authorizeAccess();
+
+        try {
+            $this->beginDatabaseTransaction();
+
+            $this->callHook('beforeValidate');
+
+            $data = $this->form->getState();
+
+            $this->callHook('afterValidate');
+
+            $data = $this->mutateFormDataBeforeCreate($data);
+
+            $this->callHook('beforeCreate');
+
+            $this->record = $this->handleRecordCreation($data);
+
+            $this->form->model($this->getRecord())->saveRelationships();
+
+            $this->callHook('afterCreate');
+        } catch (Halt $exception) {
+            $exception->shouldRollbackDatabaseTransaction()
+                ? $this->rollBackDatabaseTransaction()
+                : $this->commitDatabaseTransaction();
+
+            return;
+        } catch (Throwable $exception) {
+            $this->rollBackDatabaseTransaction();
+
+            throw $exception;
+        }
+
+        $this->commitDatabaseTransaction();
+
+        $this->getCreatedNotification()?->send();
+
+        \Illuminate\Support\Facades\Log::debug("Debug on Categories Create function", [
+            'another' => isset($another) ? $another : '-',
+            'parent_id' => $data['parent_id'] ?? null
+        ]);
+
+        if ($another) {
+            $state = [];
+            $state['parent_id'] = session()->pull('create_categories.parent_id');
+
+            // Prepare a fresh form
+            $this->record = null;
+            $this->form->model($this->getModel())->fill($state);
+
+            return;
+        }
+
+        $this->redirect($this->getRedirectUrl());
+    }
+}
